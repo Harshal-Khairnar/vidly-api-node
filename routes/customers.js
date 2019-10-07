80fb2cef6442b1df69382dca3efd0dfcd46ade55
Harshal.Khairnar <harshal9517@gmail.com>
2019-10-07 11:42:33 +0530

    post route updated for customers

--- routes/customers.js
+++ routes/customers.js
@@ -12,13 +12,13 @@ router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const customer = new Customer({
+  let customer = new Customer({
     name: req.body.name,
     isGold: req.body.isGold,
     phone: req.body.phone
   });
 
-  await customer.save();
+  customer = await customer.save();
 
   res.send(customer);
 });
