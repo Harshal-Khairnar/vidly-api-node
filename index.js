4edab56ddc90607c4f6b1fca6f2563a1af3b307a
Harshal.Khairnar <harshal9517@gmail.com>
2019-10-09 12:32:42 +0530

    completed till  token on registering

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const config = require("config");
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL Error: jwtPrivatekey is not defined");
+  process.exit(1);
+}
 
 mongoose
   .connect("mongodb://localhost/vidly", {
@@ -17,12 +23,16 @@ const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
 
 app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ routes/auth.js
@@ -0,0 +1,41 @@
+const config = require("config");
+const Joi = require("joi");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const express = require("express");
+const router = express.Router();
+
+const { User } = require("../models/user");
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password");
+
+  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
+
+  return res.send(token);
+});
+
+function validate(req) {
+  const schema = {
+    email: Joi.string()
+      .min(3)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(3)
+      .max(255)
+      .required()
+  };
+
+  return Joi.validate(req, schema);
+}
+
+module.exports = router;
